2997843949d4e59211bcf7af7004c9dfc9e4a9c6
NorbertoYTB <116083236+juhasznorbert@users.noreply.github.com>
2022-11-08 13:31:29 +0100

    Add files via upload

---
+++ hazi.py
@@ -0,0 +1,43 @@
+
+#1
+
+for x in range(20,101,1):
+    print(x)
+ 
+ #2 
+ 
+ for x in range(1,20,1):
+    print(x)
+
+szam1=int(input("Kérem a számot"))
+    
+if x %2==0:
+    print("A szám páros")
+else:
+    print("A szám páratlan")
+    
+#3 
+
+szam1=int(input("Adja meg a számot"))
+          
+if szam1 > 20:
+          print("A szám nagyobb mint 20")
+if szam1 < 20:
+          print("A szám kisebb mint 20")
+if szam1 == 20:
+          print("A két szám pontosan egyenlő")
+#4
+
+szam1=int(input("Adjon meg egy számot"))
+
+if szam1 %3==0:
+    print("A szám osztható 3-mal")
+else:
+    print("A szám nem osztható")
+    
+#5
+
+x = 5
+while x > 0:
+    x -= 1
+print (x)
